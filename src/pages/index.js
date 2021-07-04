b1e916fee3a116ebe29070ed60df562027c95208
Abin Joseph <itsmeabjs@gmail.com>
2021-07-04 18:07:46 +0530

    Done

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,20 @@
+import data from '../data.json'
 export default function Home() {
+  console.log(data);
   return (
     <div className="main">
-      <h1>Hello World</h1>
+      <h1>{data.Name}</h1>
+      <p>{data.Address.District}</p>
+      <p>{data.Address.House}</p>
+      <p>{data.Address.code}</p>
+      <br/>
+      <p>{data.Technology[0]}</p>
+      <p>{data.Technology[1]}</p>
+      <p>{data.Technology[2]}</p>
+      <p>{data.Technology[3]}</p>
+      <p>{data.Technology[4]}</p>
+      <p>{data.Technology[5]}</p>
+      <p>{data.Technology[6]}</p>
     </div>
   );
 }
