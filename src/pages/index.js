6681a740b32e8280cc5e8914acd399da7bd5f76d
Abin Joseph <itsmeabjs@gmail.com>
2021-07-04 18:12:43 +0530

    Done

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import data from '../data.json'
+import data from "../data.json";
 export default function Home() {
   console.log(data);
   return (
@@ -7,14 +7,8 @@ export default function Home() {
       <p>{data.Address.District}</p>
       <p>{data.Address.House}</p>
       <p>{data.Address.code}</p>
-      <br/>
-      <p>{data.Technology[0]}</p>
-      <p>{data.Technology[1]}</p>
-      <p>{data.Technology[2]}</p>
-      <p>{data.Technology[3]}</p>
-      <p>{data.Technology[4]}</p>
-      <p>{data.Technology[5]}</p>
-      <p>{data.Technology[6]}</p>
+      <br />
+      {data.Technology.map((techs) =><p>{techs}</p>)}
     </div>
   );
 }
