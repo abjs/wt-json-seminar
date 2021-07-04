70e230de47157fd5e2e6dad91c670166852b1761
Abin Joseph <itsmeabjs@gmail.com>
2021-07-04 18:37:09 +0530

    Done

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,24 +4,23 @@ import { useEffect, useState } from "react";
 export default function Home() {
   const [users, setUsers] = useState(null);
   useEffect(() => {
-    axios.get("https://jsonplaceholder.typicode.com/users").then((response) => {
-      setUsers(response.data);
-      console.log(response.data);
-    });
+    axios
+      .get("https://jsonplaceholder.typicode.com/comments")
+      .then((response) => {
+        setUsers(response.data);
+        console.log(response.data);
+      });
   }, []);
   return (
     <div className="main">
       {users.map((data, index) => {
         return (
           <div key={index}>
-            <br/>
+            <br />
+            <p>{data.id}</p>
             <p>{data.name}</p>
-            <p>{data.username}</p>
             <p>{data.email}</p>
-            <p>{data.address.street}</p>
-            <p>{data.address.suite}</p>
-            <p>{data.address.city}</p>
-            <p>{data.address.zipcode}</p>
+            <p>{data.body}</p>
           </div>
         );
       })}
