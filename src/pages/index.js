e784ed721da7df0d273fe2a43ed47b84d6c93fa6
Abin Joseph <itsmeabjs@gmail.com>
2021-07-04 11:20:18 +0530

    set up

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,4 @@
-import axios from "axios";
 export default function Home() {
-  axios.get("https://dog.ceo/api/breeds/image/random").then((response) => {
-    console.log(response.data);
-  });
   return (
     <div className="Main">
       <h1>Hello World</h1>
