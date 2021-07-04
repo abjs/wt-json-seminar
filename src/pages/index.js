ef78843c71ed25d042b03013a45500f173200087
Abin Joseph <itsmeabjs@gmail.com>
2021-07-04 17:52:07 +0530

    Doeg image show final

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,17 @@
 import axios from "axios";
+import { useEffect, useState } from "react";
+
 export default function Home() {
-  axios.get("https://dog.ceo/api/breeds/image/random").then((response) => {
-    console.log(response.data);
-  });
+  const [myDogImage, setMyDogImage] = useState(null);
+  useEffect(() => {
+    axios.get("https://dog.ceo/api/breeds/image/random").then((response) => {
+      setMyDogImage(response.data.message);
+      console.log(response.data.status);
+    });
+  }, []);
   return (
     <div className="Main">
-      <h1>Hello World</h1>
+      {myDogImage && <img src={myDogImage} alt="my dog image" />}
     </div>
   );
 }
