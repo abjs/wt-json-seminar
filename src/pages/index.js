9d1a1b4567c3a24763aec7beb2c1d659a355f931
Abin Joseph <itsmeabjs@gmail.com>
2021-07-04 19:01:58 +0530

    done

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,17 +4,6 @@ export default function Home() {
   return (
     <div className="main">
       <h1>{data.Name}</h1>
-      <p>{data.Address.District}</p>
-      <p>{data.Address.House}</p>
-      <p>{data.Address.code}</p>
-      <br/>
-      <p>{data.Technology[0]}</p>
-      <p>{data.Technology[1]}</p>
-      <p>{data.Technology[2]}</p>
-      <p>{data.Technology[3]}</p>
-      <p>{data.Technology[4]}</p>
-      <p>{data.Technology[5]}</p>
-      <p>{data.Technology[6]}</p>
     </div>
   );
 }
